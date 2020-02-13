8a2cabded7979ff9f71e5e3bc72ec8a2d026fa45
Helene Semb <helensem@stud.ntnu.no>
2020-02-13 11:53:22 +0100

    k

--- source/driver/timer.c
+++ source/driver/timer.c
@@ -2,26 +2,23 @@
 #ifndef __INCLUDE_TIMER_H__
 #define __INCLUDE_TIMER_H__
 
+#include <stdio.h>
+// To use time library of C
+#include <time.h>
 
-bool startTimer ()
+void delay()
 {
-    unsigned int time;
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * 3;
     
-    unsigned int count_down_time=3;
+    // Storing start time
+    clock_t start_time = clock();
     
-    
-    clock_t x_startTime=clock();  // start clock
-    time=count_down_time_in_secs;   // update timer
-    
-    while (time>0)
-    {
-    clock_t x_countTime=clock(); // update timer difference
-        time_left=count_down_time-1; // subtract to get difference
-        }
-    if (time = 0) {
-        return 1;
-    }
-
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+}
 
 #endif // #ifndef __INCLUDE_TIMER_H__
+
 
